e94f4d69e40df9cfb258e069e7f0fdc549b42628
andreas1724 <a.buschatz@gmail.com>
2026-07-07 22:27:17 +0200

    reorder solved

--- lib/widgets/row_index_scope.dart
+++ lib/widgets/row_index_scope.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
 /// Provides the current row index down the widget tree to avoid prop-drilling.
-// class RowIndexScope extends InheritedWidget {
-//   const RowIndexScope({
-//     super.key,
-//     required this.rowIndex,
-//     required super.child,
-//   });
+class RowIndexScope extends InheritedWidget {
+  const RowIndexScope({
+    super.key,
+    required this.rowIndex,
+    required super.child,
+  });
 
-//   final int rowIndex;
+  final int rowIndex;
 
-//   static int of(BuildContext context) {
-//     final scope = context.dependOnInheritedWidgetOfExactType<RowIndexScope>();
-//     assert(scope != null, 'No RowIndexScope found in context');
-//     return scope!.rowIndex;
-//   }
+  static int of(BuildContext context) {
+    final scope = context.dependOnInheritedWidgetOfExactType<RowIndexScope>();
+    assert(scope != null, 'No RowIndexScope found in context');
+    return scope!.rowIndex;
+  }
 
-//   @override
-//   bool updateShouldNotify(RowIndexScope oldWidget) =>
-//       rowIndex != oldWidget.rowIndex;
-// }
+  @override
+  bool updateShouldNotify(RowIndexScope oldWidget) =>
+      rowIndex != oldWidget.rowIndex;
+}
